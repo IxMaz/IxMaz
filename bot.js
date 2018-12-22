3032218b59fb638710cb1f79125e1c66557d71c4
IxMaz <46066303+IxMaz@users.noreply.github.com>
2018-12-22 12:00:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,6 @@ const millis = new Date().getTime() - message.guild.createdAt.getTime();
 const now = new Date();
 const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
 const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
 var embed  = new Discord.RichEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL)
 .addField("**🆔 Server ID:**", message.guild.id,true)
@@ -30,7 +29,6 @@ var embed  = new Discord.RichEmbed()
 .addField("**👥 Members**",`[${message.guild.memberCount}]`,true)
 .addField('**💬 Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
 .addField("**🌍 Others **" , message.guild.region,true)
-.addField("**🔐 Roles **",`**[${message.guild.roles.size}]** Role `,true)
 .setColor('#000000')
 message.channel.sendEmbed(embed)
  
