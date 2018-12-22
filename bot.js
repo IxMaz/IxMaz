be59bdc6e46619d76a0e37be615f066f13fc2e9e
IxMaz <46066303+IxMaz@users.noreply.github.com>
2018-12-22 11:52:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 const Discord = require('discord.js');
-const Client = discord.Client();
+const client = new Discord.Client();
 
-
-client.on('ready' () => { 
-    console.log(`${bot.user.tag}`);
-    client.user.setgame(`هنا بلاينق البوت`)
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-
-
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 
 
